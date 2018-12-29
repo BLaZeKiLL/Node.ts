5eb948d5e39352783c8712e55d9965697c1c6ff5
BLaZeKiLL <laldevashish@gmail.com>
2018-12-29 22:15:48 +0530

    App not a singelton anymore

--- src/App/app.ts
+++ src/App/app.ts
@@ -6,31 +6,32 @@ import IndexRoutes from '../Routes/index';
 
 export class App {
 
-  private static _Context: App;
-
-  public static get Context(): Express {
-    if (this._Context == undefined) {
-      this._Context = new App();
-    }
-    return this._Context.app;
-  }
-
   private app: Express;
 
-  private constructor() {
+  constructor(
+    private PORT: number
+  ) {
     this.app = express();
 
     this.setupBodyParser();
     this.setupRoutes();
   }
 
-  private setupBodyParser() {
+  public listen(callback: () => any): void {
+    this.app.listen(this.PORT, callback());
+  }
+
+  public getLocalUrl(): string {
+    return `http://localhost:${this.PORT}/`;
+  }
+
+  private setupBodyParser(): void {
     this.app.use(bodyParser.urlencoded({extended: true}));
     this.app.use(bodyParser.text());
     this.app.use(bodyParser.json({ type: 'application/json'}));
   }
 
-  private setupRoutes() {
+  private setupRoutes(): void {
     this.app.use('/', IndexRoutes);
   }
 
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
 import { App } from './App/app';
 
-App.Context.listen(3000, () => {
-    console.log('Serving on http://localhost:3000/');
+const app = new App(3000);
+
+app.listen(() => {
+  console.log(app.getLocalUrl());
 });
+
